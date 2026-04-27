1d857c34176421b769184d2e9191fea4f042f0de
Yazeed <yazeed25masadeh@gmail.com>
2026-04-27 22:25:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,7 +20,18 @@ builder.Services.AddCors(options =>
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
+
 app.UseCors("AllowMyScreen");
 
 // Configure the HTTP request pipeline.
